3a79ec0521313a540473e597664124fd0aee4d7c
Steven-Lie <s.lie01.sl@gmail.com>
2023-08-12 15:22:52 +0700

    refactor: get string from xml

--- app/src/main/java/com/steven/disaster/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/steven/disaster/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.steven.disaster.R
 import com.steven.disaster.data.ApiService
 import com.steven.disaster.data.response.DisasterResponse
 import com.steven.disaster.data.response.GeometriesItem
@@ -15,7 +16,10 @@ import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val apiService: ApiService, context: Application) :
+class MainViewModel @Inject constructor(
+    private val apiService: ApiService,
+    private val context: Application
+) :
     ViewModel() {
     private val _geometriesItem = MutableLiveData<List<GeometriesItem?>?>()
     val geometriesItem: LiveData<List<GeometriesItem?>?> = _geometriesItem
@@ -71,7 +75,11 @@ class MainViewModel @Inject constructor(private val apiService: ApiService, cont
                         if (firstResult?.observations?.isNotEmpty() == true) {
                             val firstResultStatus = firstResult.observations[0]?.f4
                             _tmaStatus.value =
-                                "$firstGaugeName berada pada status $firstResultStatus"
+                                context.getString(
+                                    R.string.tma_status,
+                                    firstGaugeName,
+                                    firstResultStatus
+                                )
                         }
                     } else {
                         _tmaStatus.value = null
